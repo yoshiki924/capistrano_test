61fc5e3e622459f73fbcd36784f32af7043c1bfe
yoshiki924 <okamura.yoshiki.0924@gmail.com>
2020-10-03 23:39:05 +0900

    capstrano

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+# デプロイするアプリケーション
+set :application, "capistrano_test"
+
+# cloneするgitリポジトリ
+set :repo_url, "git@github.com:yoshiki924/capistrano_test.git"
+
+# デプロイするブランチ（デフォルトはmasterなのでなくても良い）
+set :branch, 'master'
+
+# デプロイ先のディレクトリ
+set :deploy_to, '/var/www/capistrano'
+
+# シンボリックリンクを貼るファイル
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+
+# シンボリックリンクを貼るフォルダ
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# 保持するバージョンの個数
+set :keep_releases, 5
+
+# rubyのバージョン
+set :rbenv_ruby, '2.7.1'
+
+# 出力するログのレベル
+set :log_level, :debug
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rails, 'db:create'
+        end
+      end
+    end
+  end
+
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rails, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: groups, limit: 3, wait: 10 do
+    end
+  end
+end
